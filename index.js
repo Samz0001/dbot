e552cb5f153b5dd468ab2877a6659f9e72425592
sishere23 <shyamduaoppo@gmail.com>
2022-07-15 14:22:25 +0530

    welcome message part 2

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 require("dotenv").config()
 
 const Discord = require("discord.js")
-
+const generateImage = require("./generateImage")
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -20,4 +21,15 @@ client.on("messageCreate",(message) => {
         message.reply("Hello World!")
     }
 })
+
+const welcomeChannelid = "997146400171241473"
+
+client.on("guildMemberAdd",async (member) => {
+    const img = await generateImage(member)
+member.guild.channels.cache.get(welcomeChannelid).send({
+    content: `<@${member.id}> Welcome to the server!`,
+    files: [img]
+})
+})
+
 client.login(process.env.TOKEN)
